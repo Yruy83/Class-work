0e51a7f6faaec179764c9b209a81d65073b6edea
Yury Kanisheuski <yury.kanisheuski@gmail.com>
2023-06-01 19:26:18 +0300

    add header-block

--- Final-project/src/index.js
+++ Final-project/src/index.js
@@ -1,3 +1,6 @@
 import './main.scss';
 import './components/typography/typography.scss';
+import './components/container.scss';
+import './components/form/form.js';
+import './components/header/header.js';
 console.log('Hello IT-academy student 🚀');
