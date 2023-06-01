baa1636a145b5e166b092e0cd3181dd0a169a7f3
Yury Kanisheuski <yury.kanisheuski@gmail.com>
2023-06-02 00:26:11 +0300

    add top-panel

--- Final-project/src/components/form/form.js
+++ Final-project/src/components/form/form.js
@@ -1,6 +1,7 @@
 import './form.scss';
 
 const inputs = document.querySelectorAll('.form__input');
+const formSearchIcons = document.querySelectorAll('.form__icon');
 
 const onFocus = (evt) => {
   const label = evt.target.labels[0];
@@ -19,3 +20,16 @@ inputs.forEach((element) => {
   element.addEventListener('focus', onFocus);
   element.addEventListener('blur', onBlur);
 });
+
+const onClickIcon = (evt) => {
+  evt.preventDefault();
+  const parent = evt.currentTarget.parentElement;
+  const wrapper = parent.querySelector('.form__input-wrapper');
+  wrapper.classList.remove('form__input-wrapper--hidden');
+};
+
+formSearchIcons.forEach((element) => {
+  element.addEventListener('click', onClickIcon);
+});
+
+document.addEventListener
--- Final-project/src/index.js
+++ Final-project/src/index.js
@@ -3,4 +3,5 @@ import './components/typography/typography.scss';
 import './components/container.scss';
 import './components/form/form.js';
 import './components/header/header.js';
+import '@fortawesome/fontawesome-free/js/all';
 console.log('Hello IT-academy student 🚀');
